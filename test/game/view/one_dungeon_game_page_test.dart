9cd01a6ecaa5c91d3188d5152bb031dcd21a1802
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-08-22 17:26:30 +0300

    Created OneDungeonGamePage test.

---
+++ test/game/view/one_dungeon_game_page_test.dart
@@ -0,0 +1,65 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:one_dungeon/assets_manager/assets_manager.dart';
+import 'package:one_dungeon/game/game.dart';
+import 'package:one_dungeon/injector.dart' as di;
+
+import '../../helpers/helpers.dart';
+
+class _MockAssetsManagerCubit extends MockCubit<AssetsManagerState>
+    implements AssetsManagerCubit {}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  late _MockAssetsManagerCubit mockAssetsManagerCubit;
+
+  setUpAll(() async {
+    mockAssetsManagerCubit = _MockAssetsManagerCubit();
+
+    await di.initializeDependencies();
+    await di.injector.unregister<AssetsManagerCubit>();
+
+    di.injector
+        .registerFactory<AssetsManagerCubit>(() => mockAssetsManagerCubit);
+  });
+
+  tearDownAll(() async {
+    await di.injector.reset();
+  });
+
+  void arrangeState(AssetsManagerState state) {
+    when(() => mockAssetsManagerCubit.state).thenReturn(state);
+  }
+
+  void arrangeLoad() {
+    when(() => mockAssetsManagerCubit.load()).thenAnswer((_) async {});
+  }
+
+  group('OneDungeonGamePage', () {
+    testWidgets('renders LoadingPage', (tester) async {
+      await tester.runAsync(() async {
+        arrangeState(const AssetsManagerState.initial());
+        arrangeLoad();
+
+        await tester.pumpApp(const OneDungeonGamePage());
+        await tester.pump();
+
+        expect(find.byType(LoadingPage), findsOneWidget);
+      });
+    });
+
+    testWidgets('renders OneDungeonGameLoadedView', (tester) async {
+      await tester.runAsync(() async {
+        arrangeState(const AssetsManagerState(assetsCount: 1, loaded: 1));
+        arrangeLoad();
+
+        await tester.pumpApp(const OneDungeonGamePage());
+        await tester.pump();
+
+        expect(find.byType(OneDungeonGameLoadedView), findsOneWidget);
+      });
+    });
+  });
+}
--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:one_dungeon/assets_manager/assets_manager.dart';
+import 'package:one_dungeon/constants/constants.dart';
+import 'package:one_dungeon/injector.dart' as di;
 import 'package:one_dungeon/l10n/l10n.dart';
+import 'package:one_dungeon/start_game/start_game.dart';
 
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(Widget widget) {
+  Future<void> pumpApp(
+    Widget widget, {
+    AssetsManagerCubit? assetsManagerCubit,
+  }) async {
     return pumpWidget(
-      MaterialApp(
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
+      MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => di.injector<AssetsManagerCubit>()..load(),
+          ),
+          BlocProvider(create: (_) => di.injector<StartGameCubit>()),
         ],
-        supportedLocales: AppLocalizations.supportedLocales,
-        home: widget,
+        child: MaterialApp(
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: widget,
+        ),
       ),
     );
   }
